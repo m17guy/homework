991dcc9c042419d13bd089a2e4d50a989fe71137
m17guy <m1734guy@gmail.com>
2018-07-01 21:11:29 +0200

    1.7.18

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            hwS();
+            LU();
+            RU();
+            egapt();
+            Console.ReadLine();
+        }
+
+        static void LU()
+        {
+            Console.WriteLine("hight?");
+            int hight = int.Parse(Console.ReadLine());
+            string a = " ";
+            for (int b = 1; b <= hight; b++)
+            {
+                Console.WriteLine("*");
+                Console.Write(a);
+                a += " ";
+
+            }
+        }
+
+        static void RU()
+        {
+            Console.WriteLine("hight?");
+            int hight = int.Parse(Console.ReadLine());
+            for (int a = 0; a < hight; a++)
+            {
+                for (int i = hight - a; i > 1; i--)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+
+        static void egapt()
+        {
+            Console.WriteLine("hight?");
+            int hight = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= hight; i++)
+            {
+                for (int line = hight; line > i; line--)
+                {
+                    Console.Write(" ");
+                }
+                for (int line = 0; line < i; line++)
+                {
+                    Console.Write("*");
+                    if (line>=1)
+                        Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void hwS()
+        {
+            Console.WriteLine("[] size?");
+            int side = int.Parse(Console.ReadLine());
+            bool inside = true;
+            for (int i = 1; i < 3; i++)
+            {
+                for (int a = 1; a <= side; a++)
+                {
+                    if (i<=1||side>1)
+                        Console.Write("*");
+                }
+                Console.WriteLine();
+                if (inside == true)
+                {
+                    for (int c = 1; c <= side - 2; c++)
+                    {
+                        Console.Write("*");
+                        for (int b = 1; b <= side - 2; b++)
+                        {
+                            Console.Write(" ");
+                        }
+                        Console.WriteLine("*");
+                    }
+                }
+                inside = false;
+            }
+        }
+    }
+}

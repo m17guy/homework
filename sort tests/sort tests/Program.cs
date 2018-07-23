da2f395da727c91f9b278e3520696d48d36aa379
guy m <m1734guy@gmail.com>
2018-07-23 15:34:20 +0300

    tryed to improve sort3, needs more work

--- sort tests/sort tests/Program.cs
+++ sort tests/sort tests/Program.cs
@@ -10,11 +10,11 @@ namespace sort_tests
     {
         static void Main(string[] args)
         {
-            int[] arr = { 5, 2, -4, 3, 1, 9, 6 };
+            int[] arr = { 5, 4, 3, 3, 1, 9, 6 };
             if (arr.Min() > 0)
-                arr = sort3(arr);
+                arr = sort32(arr);
             else
-                arr = sort2(arr);
+                arr = sort12(arr);
             foreach(int i in arr)
                 Console.WriteLine(i);
             Console.ReadKey();
@@ -38,7 +38,7 @@ namespace sort_tests
             }
             return tofix;
         }
-        static int[] sort2(int[] tofix)
+        static int[] sort12(int[] tofix)
         {
             bool wrong = true;
             int count = 0;
@@ -74,6 +74,58 @@ namespace sort_tests
                 {
                     give[count] = i;
                     count++;
+                }
+            }
+            return give;
+        }
+        static int[] sort32(int[] get)
+        {
+            int count = 0;
+            int[] witho = new int[get.Max()+count], give = new int[get.Length];
+            //int[] withoneg = new int[get.Min() * -1];
+            foreach (int i in get)
+            {
+                int lok = i - 1;
+                if (witho[lok] != 0)
+                    count++;
+                else
+                    witho[lok] = i;
+            }
+            if (count > 0)
+            {
+                int[] newwitho = new int[witho.Length + count];
+                count = 0;
+                foreach (int i in get)
+                {
+                    int lok = i - 1;
+                    if (newwitho[lok] != 0)
+                    {
+                        count++;
+                        newwitho[lok + count] = i;
+                    }
+                    else
+                        newwitho[lok] = i;
+                }
+                count = 0;
+                foreach (int i in newwitho)
+                {
+                    if (i != 0) //the if to to get it the right size
+                    {
+                        give[count] = i;
+                        count++;
+                    }
+                }
+            }
+            else
+            {
+                count = 0;
+                foreach (int i in witho)
+                {
+                    if (i != 0)
+                    {
+                        give[count] = i;
+                        count++;
+                    }
                 }
             }
             return give;

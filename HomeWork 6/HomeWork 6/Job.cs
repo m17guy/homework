1816c06e705a9024e3d765ffcd0ca4c5ca33eca7
guy m <m1734guy@gmail.com>
2018-08-08 01:55:52 +0300

    8.8.18

---
+++ HomeWork 6/HomeWork 6/Job.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_6
+{
+    class Job
+    {
+        public string name;
+        public string descripton;
+        public double salary;
+        public int vacationdays;
+        public Job()
+        {
+            void promotion(double newsalary)
+            {
+                if(newsalary>salary)
+                    salary = newsalary;
+                //salary = (newsalary > salary) ? newsalary : salary;
+            }
+            void takevacation(int days)
+            {
+                if (vacationdays > days)
+                    vacationdays -= days;
+                //vacationdays = (vacationdays > days) ? (vacationdays - days) : vacationdays;
+            }
+        }
+    }
+}
---
+++ HomeWork 6/HomeWork 6/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_6
+{
+    class Person
+    {
+        public string name = "tod";
+        public int age;
+        public bool isadult;
+        public Person()
+        {
+            void bday()
+            {
+                age++;
+                isadult = (age >= 18);
+            }
+        }
+        public void newname(string setname)
+        {
+            name = setname;
+        }
+    }
+}

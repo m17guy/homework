f962671a3915c1e3bcb2ecbc5d144e76e1616d66
guy m <m1734guy@gmail.com>
2018-11-11 18:56:47 +0200

    suprizingly intresting question

---
+++ HomeWork11/p2/p2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace p2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int avr = 0, counter = 0;
+            string all="", num="";
+            while (num != "stop")
+            {
+                num=getnum();
+                all += num.ToLower()+".";
+                if (num != "stop")
+                {
+                    avr += int.Parse(num);
+                }
+                counter++;
+            }
+            printall(all, avr / (counter - 1));
+            Console.ReadLine();
+        }
+
+        static string getnum()
+        {
+            Console.WriteLine("enter num:");
+            return Console.ReadLine();
+        }
+        static void printall(string it, int av)
+        {
+            for(int i = 0; it[i] != 's'; i++)
+            {
+                string corint = "";
+                while(it[i] != '.')
+                {
+                    corint += it[i];
+                    i++;
+                }
+                
+                if (int.Parse(corint)>av)
+                    Console.WriteLine(corint);
+
+            }
+        }
+    }
+}

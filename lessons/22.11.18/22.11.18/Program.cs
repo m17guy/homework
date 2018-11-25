19803862ad0b32668d41af6efc7eee0e4df2f98c
guy m <m1734guy@gmail.com>
2018-11-25 17:47:21 +0200

    homework

--- lessons/22.11.18/22.11.18/Program.cs
+++ lessons/22.11.18/22.11.18/Program.cs
@@ -9,81 +9,8 @@ namespace _22._11._18
         static void Main(string[] args)
         {
             //ex1();
-            //ex2:
-            string input = "", inAt, inSco;
-            List<string> students = new List<string>();
-            Dictionary<string, List<string>> atributs = new Dictionary<string, List<string>>();
-
-            while (input != "1")
-            {
-                Console.WriteLine("1. stop\n2. add student\n3. add atribut to student\n4. add num val to atribut");
-                input = Console.ReadLine();
-                switch (input)
-                {
-                    case "1":
-                        break;
-                    case "2":
-                        students.Add(input);
-                        break;
-                    case "3":
-                        Console.WriteLine("student:");
-                        input = Console.ReadLine();
-                        Console.WriteLine("atribut:");
-                        inAt = Console.ReadLine();
-                        if (!atributs.ContainsKey(input))
-                        {
-                            atributs.Add(input, new List<string>());
-                        }
-                        atributs[input].Add(inAt);
-                        break;
-                    case "4":
-                        Console.WriteLine("student:");
-                        input = Console.ReadLine();
-                        Console.WriteLine("atribut:");
-                        inAt = Console.ReadLine();
-                        Console.WriteLine("score:");
-                        inSco = Console.ReadLine();
-                        atributs[input][atributs[input].IndexOf(inAt)] += inSco;
-                        break;
-                }
-            }
-            input = "";
-            while (input != "1")
-            {
-                Console.WriteLine("1. stop\n2. get student list\n3. get atribut of student\n4. get num val of atribut");
-                input = Console.ReadLine();
-                switch (input)
-                {
-                    case "1":
-                        break;
-                    case "2":
-                        foreach(string s in students)
-                        {
-                            Console.WriteLine(s);
-                        }
-                        break;
-                    case "3":
-                        Console.WriteLine("student:");
-                        input = Console.ReadLine();
-                        Console.WriteLine("atribut:");
-                        inAt = Console.ReadLine();
-                        if (!atributs.ContainsKey(input))
-                        {
-                            atributs.Add(input, new List<string>());
-                        }
-                        atributs[input].Add(inAt);
-                        break;
-                    case "4":
-                        Console.WriteLine("student:");
-                        input = Console.ReadLine();
-                        Console.WriteLine("atribut:");
-                        inAt = Console.ReadLine();
-                        Console.WriteLine("score:");
-                        inSco = Console.ReadLine();
-                        atributs[input][atributs[input].IndexOf(inAt)] += inSco;
-                        break;
-                }
-            }
+            ex2();
+            
         }
         static void ex1()
         {
@@ -128,6 +55,123 @@ namespace _22._11._18
                 {
                     Console.WriteLine("not found");
                 }
+            }
+        }
+        static void ex2()
+        {
+            string input = "", inAt, inSco;
+            Dictionary<string, Dictionary<string, string>> atributs = new Dictionary<string, Dictionary<string, string>>();
+
+            while (input != "1")
+            {
+                Console.WriteLine("1. stop\n2. add student\n3. add atribut to student\n4. add num val to atribut");
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        Console.WriteLine("student name:");
+                        input = Console.ReadLine();
+                        atributs.Add(input, new Dictionary<string, string>());
+                        break;
+                    case "3":
+                        Console.WriteLine("student:");
+                        input = Console.ReadLine();
+                        if (!atributs.ContainsKey(input))
+                        {
+                            Console.WriteLine("no such student");
+                            Console.ReadKey();
+                            break;
+                        }
+                        Console.WriteLine("atribut name:");
+                        inAt = Console.ReadLine();
+                        if (atributs[input].ContainsKey(inAt))
+                        {
+                            break;
+                        }
+                        atributs[input].Add(inAt, null);
+                        break;
+                    case "4":
+                        Console.WriteLine("student:");
+                        input = Console.ReadLine();
+                        if (!atributs.ContainsKey(input))
+                        {
+                            Console.WriteLine("no such student");
+                            Console.ReadKey();
+                            break;
+                        }
+                        Console.WriteLine("atribut:");
+                        inAt = Console.ReadLine();
+                        if (!atributs[input].ContainsKey(inAt))
+                        {
+                            Console.WriteLine("no such atribute");
+                            Console.ReadKey();
+                            break;
+                        }
+                        Console.WriteLine("score:");
+                        inSco = Console.ReadLine();
+                        atributs[input][inAt] +=" " + inSco;
+                        break;
+                }
+                Console.Clear();
+            }
+            input = "";
+            while (input != "1")
+            {
+                Console.WriteLine("1. stop\n2. get student list\n3. get atribut list of student\n4. get num val of atribut");
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        foreach (string s in atributs.Keys)
+                        {
+                            Console.WriteLine(s);
+                        }
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        Console.WriteLine("student:");
+                        input = Console.ReadLine();
+                        if (!atributs.ContainsKey(input))
+                        {
+                            Console.WriteLine("no such student");
+                            Console.ReadKey();
+                            break;
+                        }
+                        foreach (string s in atributs[input].Keys)
+                        {
+                            Console.WriteLine(s);
+                        }
+                        Console.ReadKey();
+                        break;
+                    case "4":
+                        Console.WriteLine("student:");
+                        input = Console.ReadLine();
+                        if (!atributs.ContainsKey(input))
+                        {
+                            Console.WriteLine("no such student");
+                            Console.ReadKey();
+                            break;
+                        }
+                        Console.WriteLine("atribut:");
+                        inAt = Console.ReadLine();
+                        if (!atributs[input].ContainsKey(inAt))
+                        {
+                            Console.WriteLine("no such atribute");
+                            Console.ReadKey();
+                            break;
+                        }
+                        foreach (string s in atributs[input].Values)
+                        {
+                            Console.Write(s);
+                        }
+                        Console.ReadKey();
+                        break;
+                }
+                Console.Clear();
             }
         }
     }

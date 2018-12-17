72c1731404ebef76e3b43cdc3e1f0075bcb90c43
m17guy <m1734guy@gmail.com>
2018-12-17 19:04:56 +0200

    small

---
+++ OOP/lessons/17.12.18/short lesson/short lesson/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace short_lesson
+{
+    class Program
+    {
+        delegate double func(double num);
+        delegate bool secondfunc();
+        static void Main(string[] args)
+        {
+            Console.WriteLine(addfuncs(delegate (double x) { return x * x; }, delegate (double x) { return x * x * x; }, 5));
+            Console.WriteLine(not(delegate() { return true; }));
+        }
+        static double addfuncs(func a, func b, double c)
+        {
+            return a(c) + b(c);
+        }
+        static bool not(secondfunc x)
+        {
+            return !x();
+        }
+    }
+}

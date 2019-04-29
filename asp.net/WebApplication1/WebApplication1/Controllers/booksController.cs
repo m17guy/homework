aad2e7baf4b69ae27b9dcf11c3a2839429f62eb0
m17guy <m1734guy@gmail.com>
2019-04-29 21:00:04 +0300

    a lot to learn

---
+++ asp.net/WebApplication1/WebApplication1/Controllers/booksController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class booksController : Controller
+    {
+        // GET: books
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult bestseller()
+        {
+            book testbook = new book { author = "john scolzi", name = "old mans war", pagecount = 365 };
+            return View(testbook);
+        }
+    }
+}
---
+++ asp.net/WebApplication1/WebApplication1/Models/book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class book
+    {
+        public string name;
+        public string author;
+        public int pagecount;
+    }
+}

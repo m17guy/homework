ccd26a755104806ca4d8c7f587e1fe5c05188a8a
m17guy <m1734guy@gmail.com>
2019-05-20 21:04:23 +0300

    bad but as bad as last time

---
+++ asp.net/simpletest/simpletest/Controllers/aController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simpletest.Controllers
+{
+    public class aController
+    {
+        public string goA()
+        {
+            return "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        }
+    }
+}
---
+++ asp.net/simpletest/simpletest/Controllers/bController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simpletest.Controllers
+{
+    public class bController
+    {
+        public string goB()
+        {
+            return "bbbbbbbbbbbbbbbbbbbbbbbb";
+        }
+    }
+}

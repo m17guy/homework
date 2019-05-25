156e1291d74e0645d74af559466f5d568cad99d6
guy m <m1734guy@gmail.com>
2019-05-25 14:17:35 +0300

    hw

---
+++ asp.net/23.5.19/homework/homework/Controllers/aController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace homework.Controllers
+{
+    public class aController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult power(double p, double b)
+        {
+            double resolt = Math.Pow(b, p);
+            ViewBag.r = resolt;
+            return View();
+        }
+        public IActionResult bet(int min, int max)
+        {
+            int[] all = new int[max - min - 1];
+            for(int i = 0; i < all.Length; i++)
+            {
+                all[i] = ++min;
+            }
+            ViewBag.a = all;
+            return View();
+        }
+    }
+}
---
+++ asp.net/23.5.19/homework/homework/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace homework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

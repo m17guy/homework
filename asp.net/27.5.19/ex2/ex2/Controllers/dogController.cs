a4353d4ce5339e8342409f28b4caaae988a286b0
m17guy <m1734guy@gmail.com>
2019-05-27 21:17:23 +0300

    im very sick

---
+++ asp.net/27.5.19/ex2/ex2/Controllers/dogController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ex2.Models;
+
+namespace ex2.Controllers
+{
+    public class dogController : Controller
+    {
+        public dog[] dlist = { new dog { name = "moe", age = 9 }, new dog { name = "dvash", age = 15 }, new dog { name = "mick", age = 4 }, new dog { name = "killer", age = 6 } };
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult seedog(int id)
+        {
+            dog d = new dog();
+            return View(d);
+        }
+    }
+}
---
+++ asp.net/27.5.19/ex2/ex2/dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ex2.Controllers
+{
+    public class dog
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+        public int age { get; set; }
+    }
+}

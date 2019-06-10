f92fe711ed191a9a35d37a6947836cc07c6167de
m17guy <m1734guy@gmail.com>
2019-06-10 20:42:07 +0300

    one of the worst ones

---
+++ asp.net/10.6.19/binerytree/binerytree/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace binerytree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class node
+    {
+        public int id;
+        public bool printed;
+        public node left;
+        public node right;
+    }
+    class bitree
+    {
+        public List<node> mytree = new List<node>();
+        public void add(node n)
+        {
+            
+        }
+        public bool find(int id)
+        {
+            node temp;
+            if (mytree[0].id > id) //smaller on the left
+            {
+                temp = mytree[0].left;
+            }
+            do
+            {
+                if (mytree[counter].id > id)
+                {
+                    counter=
+                }
+                else
+                {
+                    if
+                }
+            }
+        }
+        public bool checkside(node n, string side)
+        {
+            if (side == "r" || side == "right")
+            {
+                if (n.right != null)
+                    return true;
+            }
+            if (side == "l" || side == "left")
+            {
+                if (n.left != null)
+                    return true;
+            }
+            return false;
+        }
+        public void print()
+        {
+
+        }
+    }
+}
---
+++ asp.net/10.6.19/databasemaybe/databasemaybe/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace databasemaybe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

6c4ef2ef8e202a3c38be2eeb47673e3b23c783bf
guy m <m1734guy@gmail.com>
2019-06-13 06:23:37 +0300

    a

--- asp.net/10.6.19/binerytree/binerytree/Program.cs
+++ asp.net/10.6.19/binerytree/binerytree/Program.cs
@@ -7,41 +7,98 @@ namespace binerytree
     {
         static void Main(string[] args)
         {
-            
+            bitree tree = new bitree();
+            tree.mytree.Add(new node { id = 5 });
+            tree.mytree[0].left = new node { id = 1, perent = tree.mytree[0] };
+            tree.mytree[0].right = new node { id = 7, perent = tree.mytree[0] };
+            Console.WriteLine(tree.addon(new node { id = 8 }));
+            Console.WriteLine(tree.addon(new node { id = 99 }));
+            Console.WriteLine(tree.mytree[0].right.right.perent.id);
         }
     }
     class node
     {
         public int id;
-        public bool printed;
+        //public bool printed;
         public node left;
         public node right;
+        public node perent;
     }
     class bitree
     {
+        //public node head;
         public List<node> mytree = new List<node>();
-        public void add(node n)
+        public string addon(node n)
         {
-            
+            if (this.find(n.id))
+            {
+                return "node is already in the tree";
+            }
+            node temp = mytree[0];
+            while (true)
+            {
+                if (temp.id > n.id) //smaller on the left
+                {
+                    if (checkside(temp, "l"))
+                    {
+                        temp = temp.left;
+                    }
+                    else
+                    {
+                        n.perent = temp;
+                        temp.left = n;
+                        return "the node has been added";
+                    }
+                }
+                else //biger on the right
+                {
+                    if (checkside(temp, "r"))
+                    {
+                        temp = temp.right;
+                    }
+                    else
+                    {
+                        n.perent = temp;
+                        temp.right = n;
+                        return "the node has been added";
+                    }
+                }
+            }
+            return "error: not added";
         }
         public bool find(int id)
         {
-            node temp;
-            if (mytree[0].id > id) //smaller on the left
+            node temp= mytree[0];
+            while(true)
             {
-                temp = mytree[0].left;
-            }
-            do
-            {
-                if (mytree[counter].id > id)
+                if (temp.id == id)
                 {
-                    counter=
+                    return true;
                 }
-                else
+                if (temp.id > id) //smaller on the left
                 {
-                    if
+                    if (checkside(temp, "l"))
+                    {
+                        temp = temp.left;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else //biger on the right
+                {
+                    if (checkside(temp, "r"))
+                    {
+                        temp = temp.right;
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
             }
+            return false;
         }
         public bool checkside(node n, string side)
         {
@@ -59,7 +116,43 @@ namespace binerytree
         }
         public void print()
         {
-
+            node temp = mytree[0];
+            while (temp.left!=null)
+            {
+                temp = temp.left;
+            }// temp is now most left so it is smallest
+            
+            node steps = mytree[0];
+            while (true)
+            {
+                if (temp.id == steps.id)
+                {
+                    Console.WriteLine(temp.id);
+                }
+                if (temp.id > id) //smaller on the left
+                {
+                    if (checkside(temp, "l"))
+                    {
+                        temp = temp.left;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else //biger on the right
+                {
+                    if (checkside(temp, "r"))
+                    {
+                        temp = temp.right;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return false;
         }
     }
 }

481dc564f7491d7a4a17859e2a675601968cfd7a
m17guy <m1734guy@gmail.com>
2019-07-08 21:17:46 +0300

    he is still shit

---
+++ asp.net/8.7.19/textstuff/textstuff/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace textstuff
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup_1(object sender, StartupEventArgs e)
+        {
+            MainWindow win = new MainWindow();
+            string text= "";
+            foreach(string a in e.Args)
+            {
+                text += a+" ";
+            }
+            win.block.Text = win.Title = text;
+            win.Show();
+        }
+       
+    }
+}

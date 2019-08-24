1a840db6f04bbfcd632ca51fc995fce78b99d3a1
guy m <m1734guy@gmail.com>
2019-08-25 02:49:11 +0300

    i took weather out

--- weather-app/src/app/current-weather.ts
+++
@@ -1,9 +0,0 @@
-export class currentweather{
-    constructor(public cityname:string,
-                public country:string,
-                public temp:string,
-                public icon:string,
-                public weatherkind:string,
-                public humidity:string){  }
-    
-}

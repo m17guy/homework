d930213cb14ef2f9920a0e0164928cf82cdce39b
m17guy <m1734guy@gmail.com>
2019-09-16 20:45:42 +0300

    fun littel thing

---
+++ misc/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                Console.WriteLine("ive sentence");
+            }
+            else
+            {
+                Console.WriteLine("give sentence");
+            }
+            string mystring = Console.ReadLine();
+            Console.WriteLine("give number");
+            string mynum=Console.ReadLine();
+            if (mynum.ToLower() == "all")
+            {
+                mynum = mystring.Length.ToString();
+            }
+            for(int i = 0; i <= int.Parse(mynum); i++)
+            {
+                if (i<=mystring.Length)
+                {
+                    for (int ii = 0; ii < i; ii++)
+                    {
+                        Console.Write(mystring[ii]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("to try again press g");
+            if (Console.ReadKey().Key.ToString().ToLower()== "g")
+            {
+                Main(new string[1]);
+            }
+        }
+    }
+}
